d16ee737c9aa62b7e82e00611b6d3f1ed49edd0d
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-06-11 14:17:12 -0600

    add click listener dropdown component

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Link from 'next/link'
 
 interface DropdownProps {
@@ -11,13 +11,28 @@ interface DropdownProps {
 
 export default function Dropdown ({ items, styles, title }: DropdownProps) {
   const [isOpen, setIsOpen] = useState(false)
+  const dropdownRef = useRef<HTMLDivElement>(null)
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen)
   }
 
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if ((dropdownRef.current != null) && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleOutsideClick)
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+  }, [])
+
   return (
-    <div className='relative'>
+    <div ref={dropdownRef} className='relative'>
       <button
         onClick={toggleDropdown}
         className={`font-bold py-2 px-4 rounded ${styles}`}
@@ -25,10 +40,10 @@ export default function Dropdown ({ items, styles, title }: DropdownProps) {
         {title}
       </button>
       {isOpen && (
-        <div className='absolute bg-white rounded-md shadow-lg'>
+        <div className='absolute rounded-md shadow-lg bg-al-gray'>
           {
             items.map(({ linkTo, text }, index) => (
-              <Link key={index} href={linkTo} className='block px-4 py-2 text-gray-800 hover:bg-blue-500 hover:text-white'>
+              <Link key={index} href={linkTo} className='block px-4 py-2 text-gray-800 hover:bg-al-orange hover:text-white hover:rounded-md'>
                 {text}
               </Link>
             ))
