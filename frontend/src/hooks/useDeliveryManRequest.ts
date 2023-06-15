63519f5a91a96068666a5e2c018906a3b41bc266
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-06-15 14:53:27 -0600

    add interface to hook useDeliveryManRequests

--- frontend/src/hooks/useDeliveryManRequest.ts
+++ frontend/src/hooks/useDeliveryManRequest.ts
@@ -1,6 +1,21 @@
 import baseUrl from '@/constants/baseUrl'
 import useSWR from 'swr'
 
+interface DeliveryManRequest {
+  apellidos: string
+  correo: string
+  departamento: string
+  documento_url: string
+  estado_solicitud: string
+  fecha_solicitud: Date
+  municipio: string
+  no_celular: string
+  nombres: string
+  solicitud_repartidor_id: number
+  tiene_vehiculo: string
+  tipo_licencia: string
+}
+
 const fetcher = async (url: string) => {
   const res = await fetch(url)
   return await res.json()
@@ -10,7 +25,7 @@ const useDeliveryManRequest = () => {
   const { data, error, isLoading } = useSWR(`${baseUrl}/admin/deliveryRequests`, fetcher)
 
   return {
-    deliveryManRequests: data,
+    deliveryManRequests: data as DeliveryManRequest[],
     isLoading,
     error
   }
