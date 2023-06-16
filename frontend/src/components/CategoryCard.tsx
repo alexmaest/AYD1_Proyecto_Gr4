bc900ce53a4f3b35a8e60d3edf4b322e89aea212
DavidAMaldonadoH <dvidmaldo@gmail.com>
2023-06-16 15:03:02 -0600

    chore: create Category, Combo and Product cards

--- frontend/src/components/CategoryCard.tsx
+++ frontend/src/components/CategoryCard.tsx
@@ -1,38 +1,28 @@
 import Image from 'next/image'
-import { Product } from '@/types/interfaces'
+import { Category } from '@/types/interfaces'
 
 interface IProps {
-  product: Product
+  category: Category
   handleDelete: () => Promise<void>
   handleEdit: () => void
 }
 
-function ProductCard ({ product, handleDelete, handleEdit }: IProps) {
+function CategoryCard ({ category, handleDelete, handleEdit }: IProps) {
   return (
     <div className='flex flex-col rounded overflow-hidden bg-slate-400'>
       <div className='w-full h-[200px] flex items-center justify-center'>
         <Image
           className='object-contain w-[200px] h-[200px] self'
-          src={product.image === undefined ? '/sin-fotos.png' : product.image}
-          alt={product.name === undefined ? 'Sin nombre' : product.name}
+          src={category.image === undefined ? '/sin-fotos.png' : category.image}
+          alt={category.name === undefined ? 'Sin nombre' : category.name}
           width={200}
           height={200}
         />
       </div>
       <div className='px-6 py-4'>
         <span className='flex flex-row lg:flex-row md:flex-col'>
-          <h2 className='grow font-bold text-xl mb-2'>{product.name}</h2>
-          <p className='text-2xl font-bold text-al-black'>Q.{product.price}</p>
-        </span>
-        <p className='text-gray-700 text-base font-semibold'>
-          {product.description}
-        </p>
-      </div>
-      <div className='px-6 pt-4 pb-2'>
-        <span
-          className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'
-        >
-          #{product.category}
+          <h2 className='grow font-bold text-xl mb-2'>{category.name}</h2>
+          <p className='text-2xl font-bold text-al-black'>{category.type}</p>
         </span>
       </div>
       <div
@@ -45,4 +35,4 @@ function ProductCard ({ product, handleDelete, handleEdit }: IProps) {
   )
 }
 
-export default ProductCard
+export default CategoryCard
