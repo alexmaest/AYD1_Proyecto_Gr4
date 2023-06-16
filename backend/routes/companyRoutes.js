d0eeda3d29642a5f4054a790ab5bcc0b7e55297b
Alexis Estrada <alexis1estrada@hotmail.com>
2023-06-15 20:09:12 -0600

    Add company routing
    -Multiple endpoints baseline

---
+++ backend/routes/companyRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const companyController = require('../controllers/companyController');
+
+// Admin
+router.get('/', companyController.main);
+router.get('/controlPanel/products', companyController.products);
+router.get('/controlPanel/categories', companyController.categories);
+//router.get('/controlPanel/combos', companyController.combos);
+//router.get('/controlPanel/addProduct', companyController.addProduct);
+//router.get('/controlPanel/addCategory', companyController.addCategory);
+//router.get('/controlPanel/addCombo', companyController.addCombo);
+//router.get('/orders', adminController.orders);
+//router.get('/reports', adminController.reports);
+
+module.exports = router;
