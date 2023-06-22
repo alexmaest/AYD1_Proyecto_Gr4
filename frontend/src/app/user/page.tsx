f8e87c2ab841d6f6f5c138e16a1172559f29ba9f
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-06-22 17:11:30 -0600

    add optional chaining in categories result

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -40,10 +40,10 @@ function Page () {
           <h1 className='text-3xl font-bold text-center text-orange-400'>Categorías</h1>
           <div className='flex flex-wrap justify-center'>
             {isLoading && <p>Cargando...</p>}
-            {error === undefined ? null : <p>Hubo un error al cargar las categorías</p>}
-            {categories === undefined && error === undefined
-              ? !isLoading && <p>No se encontraron categorías</p>
-              : categories.map((category) => (
+            {error === undefined || error === null ? null : <p>Hubo un error al cargar las categorías</p>}
+            {(categories === undefined || categories?.length === 0) && error === undefined && !isLoading
+              ? <p>No se encontraron categorías</p>
+              : categories?.map((category) => (
                 <div key={category.category_id} className='flex flex-col items-center justify-center w-1/3 p-4'>
                   <Link href='user/restaurants'>
                     <img src={category.image} alt={category.description} className='w-32 h-32 object-cover rounded-full' />
