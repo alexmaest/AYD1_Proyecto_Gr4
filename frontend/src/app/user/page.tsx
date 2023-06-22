beadadb52334ab70fa0c7848db7c4c80a5d3e99b
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-06-22 14:36:39 -0600

    implement category view on users page

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,4 +1,7 @@
+'use client'
 import Navbar from '@/components/Navbar'
+import useCategories from '@/hooks/useCategories'
+import Link from 'next/link'
 
 const liItems = [
   {
@@ -28,10 +31,30 @@ const liItems = [
 ]
 
 function Page () {
+  const { categories, isLoading, error } = useCategories()
   return (
     <>
       <Navbar dropdown={undefined} liElements={liItems} />
-      <div className='text-white my-[100px]'>Usuario</div>
+      <section className='my-24'>
+        <div>
+          <h1 className='text-3xl font-bold text-center text-orange-400'>Categorías</h1>
+          <div className='flex flex-wrap justify-center'>
+            {isLoading && <p>Cargando...</p>}
+            {error === undefined ? null : <p>Hubo un error al cargar las categorías</p>}
+            {categories === undefined && error === undefined
+              ? !isLoading && <p>No se encontraron categorías</p>
+              : categories.map((category) => (
+                <div key={category.category_id} className='flex flex-col items-center justify-center w-1/3 p-4'>
+                  <Link href='user/restaurants'>
+                    <img src={category.image} alt={category.description} className='w-32 h-32 object-cover rounded-full' />
+                  </Link>
+                  <p className='text-center font-bold py-3'>{category.description}</p>
+                </div>
+              ))}
+          </div>
+        </div>
+
+      </section>
     </>
   )
 }
