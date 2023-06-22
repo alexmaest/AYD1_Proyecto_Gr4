3004d9f6f6d7c29156b5e864c5dc6bce11d693e0
Alexis Estrada <alexis1estrada@hotmail.com>
2023-06-22 14:07:14 -0600

    Add user-company endpoints
    -Search company
    -Show company products, combos and categories

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,10 @@ const userController = require('../controllers/userController');
 // User
 router.get('/:id', userController.main);
 router.get('/dashboard/categories', userController.categories);
+router.get('/dashboard/company/products/:id', userController.products);
+router.get('/dashboard/company/combos/:id', userController.combos);
+router.get('/dashboard/company/categories/:id', userController.categories);
 
+router.post('/dashboard/search', userController.search);
 
 module.exports = router;
