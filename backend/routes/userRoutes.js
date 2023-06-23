baf6583b71b01b1d52b6db57b8768963b9e0ddb0
Alexis Estrada <alexis1estrada@hotmail.com>
2023-06-23 13:37:25 -0600

    Add company info for user
    -Added company information endpoint for user interface

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,7 @@ const userController = require('../controllers/userController');
 // User
 router.get('/:id', userController.main);
 router.get('/dashboard/categories', userController.dashCategories);
+router.get('/dashboard/company/:id', userController.company);
 router.get('/dashboard/company/products/:id', userController.products);
 router.get('/dashboard/company/combos/:id', userController.combos);
 router.get('/dashboard/company/categories/:id', userController.categories);
