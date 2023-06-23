1251e8349b9545892deef40f2cb19c341860c1cd
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-06-22 22:14:08 -0600

    add changes for use layout in user page

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,40 +1,11 @@
 'use client'
-import Navbar from '@/components/Navbar'
 import useCategories from '@/hooks/useCategories'
 import Link from 'next/link'
-
-const liItems = [
-  {
-    linkTo: '/user/categories',
-    text: 'Categorías'
-  },
-  {
-    linkTo: '/user/order',
-    text: 'Pedir Producto'
-  },
-  {
-    linkTo: '/user/cart',
-    text: 'Carrito'
-  },
-  {
-    linkTo: '/user/coupons',
-    text: 'Cupones'
-  },
-  {
-    linkTo: '/user/order-history',
-    text: 'Historial de pedidos'
-  },
-  {
-    linkTo: '/user/review-deliveries',
-    text: 'Calificar entregas'
-  }
-]
 
 function Page () {
   const { categories, isLoading, error } = useCategories()
   return (
     <>
-      <Navbar dropdown={undefined} liElements={liItems} />
       <section className='my-24'>
         <div>
           <h1 className='text-3xl font-bold text-center text-orange-400'>Categorías</h1>
@@ -45,7 +16,13 @@ function Page () {
               ? <p>No se encontraron categorías</p>
               : categories?.map((category) => (
                 <div key={category.category_id} className='flex flex-col items-center justify-center w-1/3 p-4'>
-                  <Link href='user/restaurants'>
+                  <Link href={
+                      {
+                        pathname: '/user/category-companies',
+                        query: { categoryId: category.category_id }
+                      }
+                        }
+                  >
                     <img src={category.image} alt={category.description} className='w-32 h-32 object-cover rounded-full' />
                   </Link>
                   <p className='text-center font-bold py-3'>{category.description}</p>
