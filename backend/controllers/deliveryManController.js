3b4a08007ca81c26f6fc0104d1d17628e39d6974
Alexis Estrada <alexis1estrada@hotmail.com>
2023-06-22 19:20:40 -0600

    Add deliveries enpoints
    -Added delivery man location change endpoint
    -Update delivery man info request endpoint

--- backend/controllers/deliveryManController.js
+++ backend/controllers/deliveryManController.js
@@ -5,43 +5,83 @@ exports.main = (req, res) => {
 };
 
 exports.deliveryManInfoRequest = (req, res) => {
-    const { correo } = req.params;
-    
-    const query = `
-    SELECT sr.usuario_id,
+  const { correo } = req.params;
+
+  const query = `
+    SELECT sr.solicitud_repartidor_id as id,
       nombres,
       apellidos,
       u.correo,
       no_celular,
-      sr.municipio_id,
-      m.descripcion as 'municipio',
-      d.departamento_id, 
       d.descripcion as 'departamento',
-      tipo_licencia_id,
-      lc.descripcion as 'tipo_licencia',
-      fecha_solicitud,
-      es.descripcion as 'estado_solicitud',
+      m.descripcion as 'municipio',
       case when tiene_vehiculo=1 then 'si' else 'no' end as 'tiene_vehiculo',
+      lc.descripcion as 'tipo_licencia',
       documento_url
-    FROM tbl_solicitud_repartidor sr 
-    inner join tbl_usuario u on sr.usuario_id=u.usuario_id
-    inner join tbl_cat_municipio m on sr.municipio_id = m.municipio_id
-    inner join tbl_cat_departamento d on m.departamento_id= d.departamento_id
-    inner join tbl_cat_tipo_licencia_conducir  lc  on sr.tipo_licencia_id=lc.tipo_licencia_conducir_id
-    inner join tbl_cat_estado_solicitud es on sr.estado_solicitud_id = es.estados_solicitud_id
-    where u.rol_usuario_id=3 and u.habilitado=1 and u.correo = ?;
-`;
+    FROM tbl_solicitud_repartidor sr
+    INNER JOIN tbl_usuario u ON sr.usuario_id = u.usuario_id
+    INNER JOIN tbl_cat_municipio m ON sr.municipio_id = m.municipio_id
+    INNER JOIN tbl_cat_departamento d ON m.departamento_id = d.departamento_id
+    INNER JOIN tbl_cat_tipo_licencia_conducir lc ON sr.tipo_licencia_id = lc.tipo_licencia_conducir_id
+    WHERE u.rol_usuario_id = 3 AND u.habilitado = 1 AND u.correo = ?;
+  `;
 
   const values = [correo];
   db.query(query, values, (error, results) => {
     if (error) {
-      res.status(500).json({ error: 'Error en la consulta MySQL' });
+      res.status(500).json({ error: 'Error with delivery man request' });
     } else {
-      res.json(results[0]);
+      const deliveryManInfo = {
+        id: results[0].id,
+        nombres: results[0].nombres,
+        apellidos: results[0].apellidos,
+        correo: results[0].correo,
+        no_celular: results[0].no_celular,
+        departamento: results[0].departamento,
+        municipio: results[0].municipio,
+        tiene_vehiculo: results[0].tiene_vehiculo,
+        tipo_licencia: results[0].tipo_licencia,
+        documento_url: results[0].documento_url
+      };
+
+      res.json(deliveryManInfo);
     }
   });
-  
-  exports.deliveryManCurrentScore = (req, res) => {
-    res.send('Information: Delivery Man Curren score');
-  };
-  };
+};
+
+exports.changeLocation = (req, res) => {
+  const { id, description, department, municipality } = req.body;
+
+  const selectQuery = 'SELECT municipio_id FROM tbl_solicitud_repartidor WHERE solicitud_repartidor_id = ?';
+  const selectValues = [id];
+  db.query(selectQuery, selectValues, (selectError, selectResults) => {
+    if (selectError) {
+      console.error(selectError);
+      res.status(500).json({ error: 'Error retrieving origin municipality' });
+      return;
+    }
+
+    if (selectResults.length === 0) {
+      res.status(404).json({ error: 'Solicitud repartidor not found' });
+      return;
+    }
+
+    const origen_municipio_id = selectResults[0].municipio_id;
+    const insertQuery = `
+      INSERT INTO tbl_cambio_ubicacion_repartidor (origen_municipio_id, destino_municipio_id, motivo_solicitud, fecha_solicitud, estado_solicitud_id, repartidor_id)
+      VALUES (?, ?, ?, NOW(), 0, ?)
+    `;
+
+    const insertValues = [origen_municipio_id, municipality, description, id];
+    db.query(insertQuery, insertValues, (insertError, insertResults) => {
+      if (insertError) {
+        console.error(insertError);
+        res.status(500).json({ error: 'Error adding location change' });
+      } else {
+        res.status(200).json({ message: 'Location change added successfully' });
+      }
+    });
+  });
+};
+
+
--- backend/routes/deliveryManRoutes.js
+++ backend/routes/deliveryManRoutes.js
@@ -6,4 +6,6 @@ const deliveryManController = require('../controllers/deliveryManController');
 router.get('/',deliveryManController.main);
 router.get('/deliveryManInfoRequest/:correo', deliveryManController.deliveryManInfoRequest);
 
+router.post('/changeLocation', deliveryManController.changeLocation);
+
 module.exports = router;
