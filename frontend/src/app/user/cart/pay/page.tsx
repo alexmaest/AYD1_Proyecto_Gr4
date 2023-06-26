c62432afe59e086030e37cfdd1822a5c88faae67
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-06-26 10:55:03 -0600

    add pay page

---
+++ frontend/src/app/user/cart/pay/page.tsx
@@ -0,0 +1,7 @@
+export default function Page () {
+  return (
+    <section className='py-28 px-2 h-screen'>
+      <h1 className='text-2xl text-orange-600 font-bold'>Pay</h1>
+    </section>
+  )
+}
